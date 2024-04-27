2a9cd6f1eca7ed69c8c3b34d72a93fc3cf5a85be
nebirous <nebirous@hotmail.com>
2024-04-27 04:30:20 +0200

    register completed

--- client/src/components/screens/Register.js
+++ client/src/components/screens/Register.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import M from 'materialize-css'
 
 const Register = () => {
+  const navigate = useNavigate()
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
   const [email, setEmail] = useState('')
   const PostData = () => {
+    if (
+      !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email,
+      )
+    ) {
+      return M.toast({ html: 'invalid email!' })
+    }
     fetch('http://localhost:5000/register', {
       method: 'post',
       headers: { 'content-Type': 'application/json' },
@@ -16,10 +24,12 @@ const Register = () => {
         password: password,
       }),
     })
-      .then((res) => res.json())
-      .then((data) => {
+      .then(res => res.json())
+      .then(data => {
         if (data.error) {
           M.toast({ html: data.error })
+        } else {
+          navigate('/Profile')
         }
       })
   }
@@ -30,19 +40,19 @@ const Register = () => {
         type="text"
         placeholder="name"
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={e => setName(e.target.value)}
       />
       <input
         type="text"
         placeholder="email"
         value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={e => setEmail(e.target.value)}
       />
       <input
         type="password"
         placeholder="password"
         value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={e => setPassword(e.target.value)}
       />
       <button
         className="btn btn-submit waves-effect waves-light red accent-1"
